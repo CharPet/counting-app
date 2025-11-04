b1d37e4c91a1a01ccba1c904820dbc65b121add8
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-04 18:03:50 +0200

    Finished basic js exercises and added an Increment- button

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 // document.getElementById("count").innerText = 5;
 
-let count = 0;
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
 
-console.log(count);
+// console.log(myAge);
 
-let myAge = 25;
-console.log(myAge);
+// console.log(count);
+
+// let myAge = 25;
+// console.log(myAge);
+
+// -----------------------------------------
+
+// let myAge = 36;
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+// -----------------------------------------
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
